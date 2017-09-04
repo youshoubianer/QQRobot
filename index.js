04b45742867508bb770788ea41af479b863b122d
ZhaoXiaodong <you_shoubian@163.com>
2017-09-04 11:21:13 +0800

    format: format output
    format: format output

--- index.js
+++ index.js
@@ -2,22 +2,17 @@
 
 const QQRobot = require('./QQRobot');
 const co = require('co');
-const request = require('request');
 
 co(function* () {
   yield QQRobot.login();
   QQRobot.pollingLoginState(function (checkUrl) {
-    console.log('登陆成功')
     wakeUp(checkUrl);
   });
 })
 
-
 function wakeUp(checkUrl) {
   co(function* () {
     yield QQRobot.checkSig(checkUrl);
-    console.log('========= cookies =========')
-    console.log(QQRobot.cookies)
     yield QQRobot.getvfwebqq();
     yield QQRobot.login2();
     yield QQRobot.getUserFriends();
@@ -25,6 +20,5 @@ function wakeUp(checkUrl) {
     yield QQRobot.getDiscuss();
     yield QQRobot.getSelfInfo();
     yield QQRobot.poll2();
-
   })
 }
